9b5f4d17536ed4b19b4be5ee091314127998ca79
Angel Dzul <dzul093@gmail.com>
2023-06-18 16:25:13 -0500

    first commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+function x(){
+    document.getElementById('boton').src = "Despues.jpg"; 
+    document.getElementById('txt').innerHTML = "Despues"
+}
+function y(){
+    document.getElementById('boton').src = "Antes.jpg";
+    document.getElementById('txt').innerHTML = "Antes"
+}
